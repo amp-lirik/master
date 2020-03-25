f1733f7a750b2653cde50212c24e9f88b6d89f09
best-lirik-lagu <61799028+best-lirik-lagu@users.noreply.github.com>
2020-03-26 00:37:43 +0700

    Create adsd.js

---
+++ adsd.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '8c39dbf414ee6ae82c6b0fec8b43098e',
+		'format' : 'iframe',
+		'height' : 600,
+		'width' : 160,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://gadsoks.com/8c39dbf414ee6ae82c6b0fec8b43098e/invoke.js"></scr' + 'ipt>');
