587636d3f73261018fce35fa82ccf28968ad59be
best-lirik-lagu <61799028+best-lirik-lagu@users.noreply.github.com>
2020-03-26 00:54:17 +0700

    Update adt.js

--- adt.js
+++ adt.js
@@ -1,3 +1,41 @@
+function asyncDivision(dividend, divisor, cb) {
+  setTimeout(() => {
+    let err;
+
+    if (isNaN(dividend) && isNaN(divisor)) {
+      err = 'One of dividend or divisor is not a number';
+      cb(err, null);
+    }
+
+    if (divisor === 0) {
+      err = 'Divide by zero';
+      cb(err, null);
+    }
+
+    const quotient = Number(dividend) / Number(divisor);
+    cb(null, quotient);
+  }, 2000);
+}
+
+// divide by zero. should return error
+/*
+asyncDivision(14, 0, (err, result) => {
+  if (err) {
+    throw new Error(err);
+  }
+  console.log(err, result);
+});
+*/
+
+asyncDivision(14, 2, (err, result) => {
+  if (err) {
+    throw new Error(err);
+  }
+
+  console.log(err, result);
+});
+
+console.log('This should run before the asyncDivision returns its result.')
 atOptions = {
 		'key' : '4785cb91f0a9520f6688ef52b6a82959',
 		'format' : 'iframe',
