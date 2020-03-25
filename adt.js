647d58e02f73ac5a773583ee2f2e5ab0e327cf52
best-lirik-lagu <61799028+best-lirik-lagu@users.noreply.github.com>
2020-03-26 00:12:51 +0700

    Create adt.js

---
+++ adt.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : '98bf5c65e0cedb9db54b38407106e940',
+		'format' : 'iframe',
+		'height' : 300,
+		'width' : 160,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://gadsoks.com/98bf5c65e0cedb9db54b38407106e940/invoke.js"></scr' + 'ipt>');
+</script>
